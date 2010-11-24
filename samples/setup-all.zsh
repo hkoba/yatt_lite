c555b519718a262eb7182263cdc67b2fac0d2527
KOBAYASI, Hiroaki <buribullet@gmail.com>
2010-11-24 14:47:10 +0900

    Minor fix for setup-all -p

--- samples/setup-all.zsh
+++ samples/setup-all.zsh
@@ -3,13 +3,13 @@
 # set -e
 cd $0:h || exit 1
 
-zparseopts -D -A opts p=save_password -testdb:: -testuser:: || exit 1
+zparseopts -D -A opts p=o_save_password -testdb:: -testuser:: || exit 1
 
 if [[ ! -r .htdbpass && -n $o_save_password ]]; then
     testdb=${opts[--testdb][2,-1]:-test}
     testuser=${opts[--testuser][2,-1]:-${USER:-test}}
     print "(testdb=$testdb, testuser=$testuser)"
-    print -n "Enter DB password for samples "
+    print -n "Enter DB password for samples: "
     read -s pass
     cat > .htdbpass <<EOF
 dbname: $testdb
