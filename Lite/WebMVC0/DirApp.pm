1ea8dab90ca8d09ad0b8bfa8eeedd04259aaaf7b
KOBAYASI, Hiroaki <buribullet@gmail.com>
2013-04-24 20:54:19 +0900

    Fix to allow having subpath for actions

--- Lite/WebMVC0/DirApp.pm
+++ Lite/WebMVC0/DirApp.pm
@@ -51,13 +51,16 @@ sub prepare_part_handler {
   my PROP $prop = $con->prop;
 
   my ($part, $sub, $pkg, @args);
-  if ($self->{cf_use_subpath} and my $subpath = $prop->{cf_subpath}) {
-    if (my @sigil = $self->parse_request_sigil($con)) {
-      if ($sigil[0] ne 'action') {
-	croak $self->error(q|Bad request: subpath %s and sigil %s|
-			   , $subpath, terse_dump(\@sigil));
-      }
-    }
+  my ($type, $item) = $self->parse_request_sigil($con);
+
+  if (defined $type and my $subpath = $prop->{cf_subpath}) {
+    croak $self->error(q|Bad request: subpath %s and sigil %s|
+		       , $subpath, terse_dump($type, $item))
+      if $type ne 'action';
+  }
+
+  if (not defined $type
+      and $self->{cf_use_subpath} and my $subpath = $prop->{cf_subpath}) {
     my $tmpl = $trans->find_file($file) or do {
       croak $self->error("No such file: %s", $file);
     };
@@ -75,7 +78,6 @@ sub prepare_part_handler {
       unless $self->{cf_dont_map_args};
 
   } else {
-    my ($type, $item) = $self->parse_request_sigil($con);
     ($part, $sub, $pkg) = $trans->find_part_handler([$file, $type, $item]);
 
     @args = $part->reorder_cgi_params($con)
