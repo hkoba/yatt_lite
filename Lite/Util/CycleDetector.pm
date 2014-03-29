6943a75388f84eb2f2b7effa4a22a75a6b8bd34b
KOBAYASI, Hiroaki <buribullet@gmail.com>
2014-03-29 18:02:20 +0900

    refs #23 - More to fix mis detection (caused by symlink/bind-mount related path ambiguity)

--- Lite/Util/CycleDetector.pm
+++ Lite/Util/CycleDetector.pm
@@ -22,56 +22,66 @@ sub Visits () {__PACKAGE__}
 use YATT::Lite::MFields qw/nodes time/;
 
 use YATT::Lite::Types
-  ([Node => fields => [qw/path discovered finished color parent/]]);
+  ([Node => fields => [qw/fname discovered finished color parent/]]);
 use YATT::Lite::Util::Enum
   (NTYPE_ => [qw/WHITE GRAY BLACK/]
    , EDGE_ => [qw/TREE BACK FORW CROSS/]);
 
 sub start {
-  my ($pack, $path) = @_;
+  my ($pack, $fname) = @_;
   my Visits $vis = bless {}, $pack;
   $vis->{time} = 0;
-  $vis->ensure_make_node($path);
-  $vis->visit_node($path);
+  $vis->ensure_make_node($fname);
+  $vis->visit_node($fname);
   $vis;
 }
 
+sub fname2id {
+  (my Visits $vis, my $fname) = @_;
+  my ($dev, $inode) = stat($fname);
+  if (grep {$_ eq ''} $dev, $inode) {
+    $fname; # Workaround
+  } else {
+    join "_", $dev, $inode;
+  }
+}
+
 sub has_node {
-  (my Visits $vis, my $path) = @_;
-  $vis->{nodes}{$path};
+  (my Visits $vis, my $fname) = @_;
+  $vis->{nodes}{$vis->fname2id($fname)};
 }
 
 sub ensure_make_node {
   (my Visits $vis, my @path) = @_;
-  foreach my $path (@path) {
-    next if $vis->{nodes}{$path};
-    $vis->make_node($path);
+  foreach my $fname (@path) {
+    next if $vis->{nodes}{$vis->fname2id($fname)};
+    $vis->make_node($fname);
   }
   @path;
 }
 
 sub make_node {
-  (my Visits $vis, my ($path)) = @_;
-  $vis->{nodes}{$path} = my Node $node = {};
-  $node->{path} = $path;
+  (my Visits $vis, my ($fname)) = @_;
+  $vis->{nodes}{$vis->fname2id($fname)} = my Node $node = {};
+  $node->{fname} = $fname;
   $node->{color} = NTYPE_WHITE;
   $node;
 }
 
 sub visit_node {
-  (my Visits $vis, my ($path, $parent)) = @_;
-  my Node $node = $vis->{nodes}{$path}
-    or croak "No such path in visits! $path";
+  (my Visits $vis, my ($fname, $parent)) = @_;
+  my Node $node = $vis->{nodes}{$vis->fname2id($fname)}
+    or croak "No such path in visits! $fname";
   $node->{color} = NTYPE_GRAY;
   $node->{discovered} = ++$vis->{time};
-  $node->{parent} = $vis->{nodes}{$parent} if $parent;
+  $node->{parent} = $vis->{nodes}{$vis->fname2id($parent)} if $parent;
   $node;
 }
 
 sub finish_node {
-  (my Visits $vis, my $path) = @_;
-  my Node $node = $vis->{nodes}{$path}
-    or croak "No such path in visits! $path";
+  (my Visits $vis, my $fname) = @_;
+  my Node $node = $vis->{nodes}{$vis->fname2id($fname)}
+    or croak "No such path in visits! $fname";
   $node->{color} = NTYPE_BLACK;
   $node->{finished} = ++$vis->{time};
   $node;
@@ -79,7 +89,7 @@ sub finish_node {
 
 sub check_cycle {
   (my Visits $vis, my ($to, $from)) = @_;
-  my Node $dest = $vis->{nodes}{$to}
+  my Node $dest = $vis->{nodes}{$vis->fname2id($to)}
     or croak "No such path in visits! $to";
   if ($dest->{color} == NTYPE_WHITE) {
     # tree edge
@@ -98,7 +108,7 @@ sub list_cycle {
   my @path;
   while ($node and $node->{parent}) {
     $node = $node->{parent};
-    push @path, $node->{path};
+    push @path, $node->{fname};
   }
   @path;
 }
