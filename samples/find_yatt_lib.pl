5cfaee8e7e8ab4a9e784eb4044c6696941f88fdf
Kodama Reon <rkodama@ssri.com>
2017-06-27 17:39:47 +0900

    For [GH #99] lib/YATT (and extlib) detector

---
+++ samples/find_yatt_lib.pl
@@ -0,0 +1,31 @@
+use strict;
+use warnings;
+use File::Basename ();
+use File::Spec;
+use Cwd ();
+
+use constant DEBUG_FIND_YATT_LIB => $ENV{DEBUG_FIND_YATT_LIB};
+
+my $thisdir = do {
+  if (-r __FILE__) {
+    # detect where app.psgi is placed.
+    File::Basename::dirname(File::Spec->rel2abs(__FILE__));
+  } else {
+    # older uwsgi do not set __FILE__ correctly, so use cwd instead.
+    Cwd::cwd();
+  }
+};
+print STDERR "# thisdir=$thisdir\n" if DEBUG_FIND_YATT_LIB;
+
+my ($found) = $thisdir =~ m{^(.*?/)YATT/}
+  or return;
+print STDERR "# found=$found\n" if DEBUG_FIND_YATT_LIB;
+
+my @libdir = $found;
+
+if (-d (my $dn = File::Basename::dirname($found) . "/extlib")) {
+  push @libdir, $dn;
+}
+print STDERR "# libdir=@libdir\n" if DEBUG_FIND_YATT_LIB;
+
+return @libdir;
