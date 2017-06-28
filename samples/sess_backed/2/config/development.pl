c6e0fc09046ade0928fbf13b2b6da281c0c97e4b
Kobayasi, Hiroaki <buribullet@gmail.com>
2017-06-28 11:14:41 +0900

    For [GH #95] [GH #97] - ensure sqlite db is group writable.

--- samples/sess_backed/2/config/development.pl
+++ samples/sess_backed/2/config/development.pl
@@ -8,8 +8,11 @@ my $dbpath = "$FindBin::Bin/var/db/site.db";
 my $schema = "$FindBin::Bin/sql/site.schema.sql";
 
 my $connector = sub {
-  DBI->connect("dbi:SQLite:dbname=$dbpath", '', ''
-               , +{sqlite_unicode => 1})
+  my $was_missing = ! -e $dbpath;
+  my $dbh = DBI->connect("dbi:SQLite:dbname=$dbpath", '', ''
+                         , +{sqlite_unicode => 1});
+  chmod 0664, $dbpath if $was_missing;
+  $dbh;
 };
 
 {
