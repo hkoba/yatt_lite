3ea47c04c9beb5af3d81ab294cbe27bed89c52d1
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-03-21 15:55:33 +0900

    WIP, not tested - For [GH #161] - port mainloop

--- Lite/LanguageServer/Generic.pm
+++ Lite/LanguageServer/Generic.pm
@@ -5,14 +5,17 @@ use warnings;
 use File::AddInc;
 use MOP4Import::Base::CLI_JSON -as_base
   , [fields =>
-       , qw/_buffer/
+       , qw/_buffer _out_semaphore/
        , [read_fd => default => 0]
        , [write_fd => default => 1]
        , [read_length => default => 8192]
+       , [jsonrpc_version => default => '2.0']
      ];
 
 use MOP4Import::Types
   (Header => [[fields => qw/Content-Length/]]);
+
+use YATT::Lite::LanguageServer::Protocol;
 
 # Most logics are shamelessly stolen from Perl::LanguageServer
 
@@ -20,11 +23,95 @@ use Coro ;
 use Coro::AIO ;
 use AnyEvent;
 
+use Scope::Guard;
+
+#========================================
+
+sub after_after_new {
+  (my MY $self) = @_;
+  $self->{_out_semaphore} = Coro::Semaphore->new;
+}
+
+sub call_method {
+  (my MY $self, my Request $request) = @_;
+  my $method = $self->translate_method_name($request->{method});
+  if (my $sub = $self->can($method)) {
+    $sub->($self, $request);
+  } else {
+    print STDERR "# Not implemented: ", $self->cli_encode_json($request), "\n";
+  }
+}
+
+sub translate_method_name {
+  (my MY $self, my $method) = @_;
+  $method =~ s,/,__,g;
+  $method =~ s,^\$,__ext__,;
+  $method;
+}
+
+sub server {
+  (my MY $self) = @_;
+  my %request; # XXX: should be an instance member?
+  while (1) {
+    my $reqRaw = $self->read_raw_request;
+    my Request $request = JSON::decode_json($reqRaw);
+
+    if (my $id = $request->{id}) {
+      $request{$id} = async {
+        my $guard = guard {
+          delete $request{$id};
+        };
+        $self->process_request($id, $request);
+      };
+    } else {
+      # XXX: notification
+    }
+  }
+}
+
+#========================================
+
+sub process_request {
+  (my MY $self, my $id, my Request $request) = @_;
+  my Response $outdata;
+  eval {
+    $outdata->{result} = $self->call_method($request);
+  };
+  if (my $msg = $@) {
+    $outdata->{error} = my Error $error = {};
+    $error->{code} = -32001;
+    $error->{message} = $msg;
+  }
+  if ($outdata) {
+    $self->emit_response($outdata, $request->{id});
+  }
+}
+
+sub emit_response {
+  (my MY $self, my Response $response, my $id) = @_;
+  $response->{id} = $id;
+  $response->{jsonrpc} = $self->{jsonrpc_version};
+
+  my $wdata = $self->make_response($self->cli_encode_json($response));
+
+  my $guard = $self->{_out_semaphore}->guard;
+  my $sum = 0;
+  use bytes;
+  while ($sum < length $wdata) {
+    my $cnt = aio_write $self->{write_fd}, undef, undef, $wdata, $sum;
+    die "write_error ($!)" if $cnt <= 0;
+    $sum += $cnt;
+  }
+
+  print STDERR "# sent response\n" unless $self->{quiet};
+}
+
 sub make_response {
   (my MY $self, my $outdata) = @_;
   if (Encode::is_utf8($outdata)) {
     Encode::_utf8_off($outdata);
   }
+  use bytes;
   my $len = length $outdata;
   my @out = ("Content-Length: $len"
                , "Content-Type: application/vscode-jsonrpc; charset=utf-8"
---
+++ Lite/LanguageServer/Protocol.pm
@@ -0,0 +1,29 @@
+package YATT::Lite::LanguageServer::Protocol;
+use strict;
+use warnings;
+
+# Import 'import' to make types exportable.
+use MOP4Import::Declare::Type -as_base;
+
+use MOP4Import::Types
+  (Message => [[fields => qw/jsonrpc/],
+               ['subtypes',
+                Request  => [[fields => qw/id method params/]],
+                Response => [[fields => qw/id result error/]],
+                Notification => [[fields => qw/method params/]],
+              ]],
+   Error => [[fields => qw/code message data/]],
+
+   CancelParams => [[fields => qw/id/]],
+   Position => [[fields => qw/line character/]],
+   Range => [[fields => qw/start end/]],
+   Location => [[fields => qw/uri range/]],
+   LocationLink => [[fields => qw/originSectionRange targetUri targetRange targetSelectionRange/]],
+
+   SignatureHelp => [[fields => qw/signatures activeSignature activeParameter/]],
+   SignatureInformation => [[fields => qw/label documentation parameters/]],
+   ParameterInformation => [[fields => qw/label documentation/]],
+  );
+
+
+1;
