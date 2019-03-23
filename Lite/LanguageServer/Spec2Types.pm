28925268045e056101c7a8d5d26afc9ecadd9d1d
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-03-23 18:05:05 +0900

    For [GH #161] - make_typespec_from
    ```shell
    % ./Lite/LanguageServer/Spec2Types.pm make_typespec_from "$(
         ./Lite/LanguageServer/Spec2Types.pm --flatten gather_interfaces "${(f@)$(
             ./Lite/LanguageServer/SpecParser.pm  --flatten parse_statement_list "$(
                 ./Lite/LanguageServer/SpecParser.pm cli_array "${(f@)$(
                     ./Lite/LanguageServer/SpecParser.pm --flatten tokenize_statement_list "$(
                           Lite/LanguageServer/SpecParser.pm  extract_statement_list "$(
                              Lite/LanguageServer/SpecParser.pm extract_codeblock typescript ./Lite/LanguageServer/specification.md)")")}")")}")" RequestMessage
    ["Message",[["fields","jsonrpc"],["subtypes","RequestMessage",[["fields",["id","doc","The request id."],["method","doc","The method to be invoked."],["params","doc","The method's params."]]]]]]
    ```

--- Lite/LanguageServer/Spec2Types.pm
+++ Lite/LanguageServer/Spec2Types.pm
@@ -7,28 +7,53 @@ use MOP4Import::Base::CLI_JSON -as_base;
 
 use YATT::Lite::LanguageServer::SpecParser qw/Interface Decl/;
 
+sub make_typespec_from {
+  (my MY $self, my ($typeDict, @names)) = @_;
+  map {
+    $self->interface2typespec($typeDict->{$_}, $typeDict);
+  } @names;
+}
+
 sub interface2typespec {
-  (my MY $self, my Interface $if) = @_;
-  if ($if->{extends}) {
-    ...
-  } else {
-    [$if->{name} => [fields => map {
-      my Decl $slotDecl = $_;
-      if ($slotDecl->{kind}) {
-        Carp::croak "Not implemented for interface body: $slotDecl->{kind}";
-      } elsif ($slotDecl->{deprecated}) {
-        ();
+  (my MY $self, my Interface $if, my $typeDict) = @_;
+  # Type is not used currently.
+  my @spec = [fields => map {
+    my Decl $slotDecl = $_;
+    if (ref $slotDecl eq 'ARRAY') {
+      my ($name, @typeunion) = @$slotDecl;
+      $name;
+    } elsif ($slotDecl->{kind}) {
+      Carp::croak "Not implemented for interface body: $slotDecl->{kind}";
+    } elsif ($slotDecl->{deprecated}) {
+      ();
+    } else {
+      my $name = $slotDecl->{body}[0];
+      $name =~ s/\?\z//;
+      if ($slotDecl->{comment}) {
+        [$name => doc => $slotDecl->{comment}];
       } else {
-        my $name = $slotDecl->{body}[0];
-        $name =~ s/\?\z//;
-        if ($slotDecl->{comment}) {
-          [$name => doc => $slotDecl->{comment}];
-        } else {
-          $name
-        }
+        $name
       }
-    } @{$if->{body}}]]
+    }
+  } @{$if->{body}}];
+  if ($if->{extends}) {
+    my ($superName, $superSpec)
+      = $self->interface2typespec($typeDict->{$if->{extends}}, $typeDict);
+    ($superName, [@$superSpec, [subtypes => $if->{name}, \@spec]]);
+  } else {
+    ($if->{name}, \@spec);
   }
+}
+
+sub gather_interfaces {
+  (my MY $self, my @decls) = @_;
+  my %dict;
+  foreach my Interface $if (@decls) {
+    next unless ref $if eq 'HASH';
+    next unless $if->{kind} eq 'interface';
+    $dict{$if->{name}} = $if;
+  }
+  \%dict;
 }
 
 MY->run(\@ARGV) unless caller;
