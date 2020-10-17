8d90ccdf25acaf0c7dd9f82799d069a10f9ba893
Kobayasi, Hiroaki <buribullet@gmail.com>
2020-10-17 16:20:43 +0900

    Checking in changes prior to tagging of version 0.101_101.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 40b2502..5ff9ed9 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for YATT::Lite
    
    +0.101_101 2020-10-17
    +    * GH-202 - fixed some test failures
    +
     0.101_100 2020-10-15
         * Security fixes
              - GH-197 - fix relative path traversal in path_info mode (which is not normally used until recently)

--- Lite.pm
+++ Lite.pm
@@ -4,7 +4,7 @@ use warnings qw(FATAL all NONFATAL misc);
 use 5.010; no if $] >= 5.017011, warnings => "experimental";
 
 use Carp qw(carp croak confess);
-our $VERSION = '0.101_100';
+our $VERSION = '0.101_101';
 use mro 'c3';
 
 use Scalar::Util qw/weaken/;
