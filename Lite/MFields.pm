236790464f32df251d9f1606e51883daf81e1dad
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-04-16 10:45:15 +0900

    GH-233 - drop `cf_` prefix

--- Lite/MFields.pm
+++ Lite/MFields.pm
@@ -12,18 +12,18 @@ BEGIN {
   package YATT::Lite::MFields::Decl;
   use parent qw/YATT::Lite::Object/;
   our %FIELDS = map {$_ => 1}
-    qw/cf_is cf_isa cf_required
-       cf_name cf_public_name cf_getter
-       cf_package
-       cf_default
-       cf_doc cf_label
-       cf_only_if_missing
+    qw/is isa required
+       name public_name getter
+       package
+       default
+       doc label
+       only_if_missing
       /;
 }
 
 BEGIN {
   our %FIELDS = map {$_ => Decl->new(name => $_)}
-    qw/fields cf_package known_parent/;
+    qw/_fields package _known_parent/;
 }
 
 use YATT::Lite::Util qw/globref look_for_globref list_isa fields_hash
@@ -41,10 +41,10 @@ sub import {
 
 sub configure_package {
   (my MY $self, my $pack) = @_;
-  $self->{cf_package} = $pack;
+  $self->{package} = $pack;
   my $sym = globref($pack, 'FIELDS');
   *$sym = {} unless *{$sym}{HASH};
-  $self->{fields} = *{$sym}{HASH};
+  $self->{_fields} = *{$sym}{HASH};
 }
 
 {
@@ -90,12 +90,12 @@ sub import_fields_from {
 	croak "Invalid item for MFields::Meta->import_fields_from: $item";
       }
       my MY $super = $item;
-      $class = $super->{cf_package};
-      next if $self->{known_parent}{$class}++;
-      $fields = $super->{fields};
+      $class = $super->{package};
+      next if $self->{_known_parent}{$class}++;
+      $fields = $super->{_fields};
     } else {
       $class = $item;
-      next if $self->{known_parent}{$class}++;
+      next if $self->{_known_parent}{$class}++;
       my $sym = look_for_globref($class, 'FIELDS')
 	or next;
       $fields = *{$sym}{HASH}
@@ -108,17 +108,17 @@ sub import_fields_from {
 	croak "Importing raw field $class.$name is prohibited!";
       }
 
-      unless (my Decl $existing = $self->{fields}->{$name}) {
-	$self->{fields}->{$name} = $importing;
+      unless (my Decl $existing = $self->{_fields}->{$name}) {
+	$self->{_fields}->{$name} = $importing;
       } elsif (not UNIVERSAL::isa($existing, $self->Decl)) {
 	croak "Importing $class.$name onto raw field"
-	  . " (defined in $self->{cf_package}) is prohibited";
-      } elsif ($importing->{cf_only_if_missing}) {
+	  . " (defined in $self->{package}) is prohibited";
+      } elsif ($importing->{only_if_missing}) {
         ; # import $importing only if it is missing in target package.
       } elsif ($importing != $existing) {
 	croak "Conflicting import $class.$name"
-	  . " (defined in $importing->{cf_package}) "
-	    . "onto $existing->{cf_package}";
+	  . " (defined in $importing->{package}) "
+	    . "onto $existing->{package}";
       }
     }
   }
@@ -126,24 +126,24 @@ sub import_fields_from {
 
 sub fields {
   (my MY $self) = @_;
-  my $f = $self->{fields};
+  my $f = $self->{_fields};
   wantarray ? map([$_ => $f->{$_}], keys %$f) : $f;
 }
 
 sub has {
   (my MY $self, my $nameSpec, my @atts) = @_;
   (my $attName, @atts) = ($self->parse_field_spec($nameSpec), @atts);
-  if (my $old = $self->{fields}->{$attName}) {
-    carp "Redefinition of field $self->{cf_package}.$attName is prohibited!";
+  if (my $old = $self->{_fields}->{$attName}) {
+    carp "Redefinition of field $self->{package}.$attName is prohibited!";
   }
   unless (@atts % 2 == 0) {
-    croak "Invalid number of field spec for $self->{cf_package}.$attName";
+    croak "Invalid number of field spec for $self->{package}.$attName";
   }
   my Decl $field = $self->Decl->new(
-    name => $attName, @atts, package => $self->{cf_package}
+    name => $attName, @atts, package => $self->{package}
   );
-  if ($field->{cf_getter}) {
-    my ($name, $code) = lexpand($field->{cf_getter});
+  if ($field->{getter}) {
+    my ($name, $code) = lexpand($field->{getter});
     if (not defined $code) {
       $code = sub {$_[0]->{$attName}};
     } elsif (not ref $code) {
@@ -151,9 +151,9 @@ sub has {
     } elsif (ref $code ne 'CODE') {
       croak "field getter code must be CODE ref! for field $attName";
     }
-    *{globref($field->{cf_package}, $name)} = $code;
+    *{globref($field->{package}, $name)} = $code;
   }
-  $self->{fields}->{$attName} = $field;
+  $self->{_fields}->{$attName} = $field;
 }
 
 sub make_accessor_type {
@@ -174,14 +174,15 @@ sub make_accessor_type_glob {
 }
 
 sub parse_field_spec {
-  my $pack = shift;
-  if ($_[0] =~ m{^(\w*)\^(\w+)$}) {
-    ($1.$2, getter => $2, ($1 ? (public_name => $2) : ()));
-  } elsif ($_[0] =~ m{^cf_(\w+)$}) {
-    ($_[0], public_name => $1);
-  } else {
-    $_[0];
+  my ($pack, $spec) = @_;
+  my @atts;
+  if ($spec =~ s/\^(\w+)\z//) {
+    push @atts, getter => $1;
   }
+  if ($spec !~ m{^_}) {
+    push @atts, public_name => $spec;
+  }
+  ($spec, @atts);
 }
 
 sub add_isa_to {
